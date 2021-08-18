1249f168b549d8d7719d96a35e1acf1ce7fed949
Keshav <57292513+9tykeshav@users.noreply.github.com>
2021-08-18 17:06:08 +0530

    made user its own entity

--- revolt/models/ready.py
+++ revolt/models/ready.py
@@ -1,5 +1,6 @@
 import json
 import aiohttp
+from .user import User
 
 
 
@@ -17,30 +18,6 @@ class Ready:
         return users
 
 
-
-
-
-class User:
-    """represents a user"""
-
-    def __init__(self , user_dict) :
-        self.id = user_dict.get("_id" , None)
-        self.username = user_dict.get("username" ,None)
-        if user_dict.get("avatar" , None) :
-            #TODO THIS IS A MESS FIX IT 
-
-            self.avatar_id = user_dict["avatar"]["_id"] 
-            self.avatar_size = user_dict["avatar"]["size"]
-            self.avatar_tag = user_dict["avatar"]["tag"]
-        else :
-            self.avatar_id = None 
-            self.avatar_size = None                  
-            self.avatar_tag = None
-
-        self.relationship = user_dict.get("relationship" ,None)
-        self.badges = user_dict.get("badges" , None)
-        self.flags = user_dict.get("flags" , None)
-        self.online = user_dict.get("online" , None)
 
 
 
